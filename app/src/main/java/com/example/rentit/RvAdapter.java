6569ac6281662ddf226dcf20c1ae09b9711449bf
GitHubYogendra <yogendragowdak006@gmail.com>
2022-05-26 21:16:03 +0530

    second phase of RentIT

--- app/src/main/java/com/example/rentit/RvAdapter.java
+++ app/src/main/java/com/example/rentit/RvAdapter.java
@@ -3,6 +3,7 @@ package com.example.rentit;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -39,6 +42,31 @@ public class RvAdapter extends FirebaseRecyclerAdapter<houseRvModel,RvAdapter.my
                 .error(R.drawable.ic_baseline_account_circle_24)
                 .into(holder.img);
 
+        holder.Details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.details_page))
+                        .setExpanded(true,2000)
+                        .create();
+
+                View view = dialogPlus.getHolderView();
+                TextView Daddress = view.findViewById(R.id.Daddress);
+                TextView Dbhk = view.findViewById(R.id.DBhk);
+                TextView Dprice = view.findViewById(R.id.Dprise);
+                TextView DUrl = view.findViewById(R.id.DUrl);
+                TextView Dtype = view.findViewById(R.id.Dtype);
+
+                Daddress.setText(model.getHouseAddress());
+                Dbhk.setText(model.getHouseBHK());
+                Dprice.setText(model.getHousePrise());
+                DUrl.setText(model.getHouseUrl1());
+                Dtype.setText(model.getType());
+
+                dialogPlus.show();
+            }
+        });
+
     }
 
     @NonNull
@@ -52,6 +80,7 @@ public class RvAdapter extends FirebaseRecyclerAdapter<houseRvModel,RvAdapter.my
 
         CircleImageView img;
         TextView Address,Prise,Bhk;
+        Button Details;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -60,6 +89,9 @@ public class RvAdapter extends FirebaseRecyclerAdapter<houseRvModel,RvAdapter.my
             Address = (TextView) itemView.findViewById(R.id.ObjName);
             Prise = (TextView) itemView.findViewById(R.id.ObjPrise);
             Bhk = (TextView) itemView.findViewById(R.id.ObjBhk);
+
+            Details = (Button)itemView.findViewById(R.id.ShowDetails);
+
         }
     }
 }
