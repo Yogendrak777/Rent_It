e2e31701cefb454427daca654bae6c5e45c05df5
GitHubYogendra <yogendragowdak006@gmail.com>
2022-06-01 07:46:58 +0530

    second phase of RentIT(v 1.0.8.2)

--- app/src/main/java/com/example/rentit/HouseInfo2.java
+++ app/src/main/java/com/example/rentit/HouseInfo2.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 public class HouseInfo2 extends AppCompatActivity {
 
-    String parking,BHK,WATER,FLOOR,FACING;
+    String parking,BHK,WATER,FLOOR,FACING,BATHROOMS,FAMILY,FOOD,PET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,10 @@ public class HouseInfo2 extends AppCompatActivity {
         Spinner water = (Spinner) findViewById(R.id.waterSpinner);
         Spinner floor = (Spinner) findViewById(R.id.FloorSpinner);
         Spinner Facing = (Spinner) findViewById(R.id.FacingSpinner);
+        Spinner Bathrooms = (Spinner)findViewById(R.id.BathroomsSpinner);
+        Spinner Family = (Spinner)findViewById(R.id.FamilySpinner);
+        Spinner Food = (Spinner)findViewById(R.id.FoodTypeSpinner);
+        Spinner Pet = (Spinner)findViewById(R.id.PetsSpinner);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.parking, android.R.layout.simple_spinner_item);
@@ -98,6 +102,70 @@ public class HouseInfo2 extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 FACING = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(HouseInfo2.this, FACING, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,R.array.bathrooms, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Bathrooms.setAdapter(adapter5);
+        Bathrooms.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                BATHROOMS = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(HouseInfo2.this, BATHROOMS, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,R.array.Family, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Family.setAdapter(adapter6);
+        Family.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                FAMILY = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(HouseInfo2.this, FAMILY, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(this,R.array.Food, android.R.layout.simple_spinner_item);
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Food.setAdapter(adapter7);
+        Food.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                FOOD = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(HouseInfo2.this, FOOD, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter8 = ArrayAdapter.createFromResource(this,R.array.Pet, android.R.layout.simple_spinner_item);
+        adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Pet.setAdapter(adapter8);
+        Pet.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                PET = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(HouseInfo2.this, PET, Toast.LENGTH_SHORT).show();
             }
 
             @Override
