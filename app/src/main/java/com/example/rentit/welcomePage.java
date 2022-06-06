3a90bcbaa686e45eaecd14ee72e18a63d187f97b
Nithin R <nithinr101010@gmail.com>
2022-06-06 21:50:57 +0530

    Search Updation and Startup Screen

--- app/src/main/java/com/example/rentit/welcomePage.java
+++ app/src/main/java/com/example/rentit/welcomePage.java
@@ -6,30 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class welcomePage extends AppCompatActivity {
-    TextView appNamelogo;
-    LottieAnimationView lottieAnimationViewStartUp;
+    //TextView appNamelogo;
+    //LottieAnimationView lottieAnimationViewStartUp;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
 
-        appNamelogo = findViewById(R.id.welcome);
-        lottieAnimationViewStartUp = findViewById(R.id.lottie);
+        //appNamelogo = findViewById(R.id.welcome);
+        //lottieAnimationViewStartUp = findViewById(R.id.lottie);
+        //lottieAnimationViewStartUp.animate().translationX(450).setDuration(3000).setStartDelay(0);
+        //appNamelogo.animate().translationY(-1500).setDuration(2000).setStartDelay(0);
 
-        lottieAnimationViewStartUp.animate().translationX(450).setDuration(3000).setStartDelay(0);
-        appNamelogo.animate().translationY(-1500).setDuration(2000).setStartDelay(0);
-
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.logo);
+        videoView.start();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(welcomePage.this,loginPage.class));
                 finish();
             }
-        },5000);
+        },2000);
     }
 }
