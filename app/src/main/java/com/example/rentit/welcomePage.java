e211f1d9c4586c2a0346642fd66b5309609623c7
GitHubYogendra <yogendragowdak006@gmail.com>
2022-06-08 20:06:31 +0530

    second phase of RentIT(v 2.1.3)

--- app/src/main/java/com/example/rentit/welcomePage.java
+++ app/src/main/java/com/example/rentit/welcomePage.java
@@ -6,23 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class welcomePage extends AppCompatActivity {
-    TextView appNamelogo;
-    LottieAnimationView lottieAnimationViewStartUp;
+//    VideoView appNameLogo;
+//    LottieAnimationView lottieAnimationViewStartUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
 
-        appNamelogo = findViewById(R.id.welcome);
-        lottieAnimationViewStartUp = findViewById(R.id.lottie);
+        //appNameLogo = findViewById(R.id.videoView);
+        //lottieAnimationViewStartUp = findViewById(R.id.lottie);
 
-        lottieAnimationViewStartUp.animate().translationX(450).setDuration(3000).setStartDelay(0);
-        appNamelogo.animate().translationY(-1500).setDuration(2000).setStartDelay(0);
+        //lottieAnimationViewStartUp.animate().translationX(450).setDuration(3000).setStartDelay(0);
+        //appNameLogo.animate().translationY(-1500).setDuration(2000).setStartDelay(0);
 
         new Handler().postDelayed(new Runnable() {
             @Override
