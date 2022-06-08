374d4c38d26f0fec04bddf4ee33c904caee18d29
Nithin R <nithinr101010@gmail.com>
2022-06-08 20:28:38 +0530

    Update welcomePage.java

--- app/src/main/java/com/example/rentit/welcomePage.java
+++ app/src/main/java/com/example/rentit/welcomePage.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -13,7 +15,7 @@ import com.airbnb.lottie.LottieAnimationView;
 public class welcomePage extends AppCompatActivity {
 //    VideoView appNameLogo;
 //    LottieAnimationView lottieAnimationViewStartUp;
-
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +23,25 @@ public class welcomePage extends AppCompatActivity {
 
         //appNameLogo = findViewById(R.id.videoView);
         //lottieAnimationViewStartUp = findViewById(R.id.lottie);
-
         //lottieAnimationViewStartUp.animate().translationX(450).setDuration(3000).setStartDelay(0);
         //appNameLogo.animate().translationY(-1500).setDuration(2000).setStartDelay(0);
 
+        videoView = findViewById(R.id.videoView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) videoView.getLayoutParams();
+        params.width = metrics.widthPixels;
+        params.height = metrics.heightPixels;
+        params.leftMargin = 0;
+        videoView.setLayoutParams(params);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.logo);
+        videoView.start();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(welcomePage.this,loginPage.class));
                 finish();
             }
-        },5000);
+        },1500);
     }
 }
